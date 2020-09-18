b69e52be6c70380d8aecd2870ec279ba81ccd1f8
imufiid <imufiid98@gmail.com>
2020-09-18 12:13:54 +0700

    event listener button menu

--- app/src/main/java/com/mufiid/pilwali2020/fragments/BerandaFragment.kt
+++ app/src/main/java/com/mufiid/pilwali2020/fragments/BerandaFragment.kt
@@ -1,13 +1,16 @@
 package com.mufiid.pilwali2020.fragments
 
+import android.app.Activity
+import android.content.Intent
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.ImageButton
+import android.widget.Toast
 import com.mufiid.pilwali2020.R
 
-// TODO: Rename parameter arguments, choose names that match
 // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
 private const val ARG_PARAM1 = "param1"
 private const val ARG_PARAM2 = "param2"
@@ -18,7 +21,6 @@ private const val ARG_PARAM2 = "param2"
  * create an instance of this fragment.
  */
 class BerandaFragment : Fragment() {
-    // TODO: Rename and change types of parameters
     private var param1: String? = null
     private var param2: String? = null
 
@@ -35,7 +37,36 @@ class BerandaFragment : Fragment() {
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_beranda, container, false)
+        val root = inflater.inflate(R.layout.fragment_beranda, container, false)
+        val btn_pilwali = root.findViewById<ImageButton>(R.id.btn_pilwali) as ImageButton
+        val btn_tps = root.findViewById<ImageButton>(R.id.btn_tps) as ImageButton
+        val btn_monitoring = root.findViewById<ImageButton>(R.id.btn_monitor) as ImageButton
+        val btn_blangko = root.findViewById<ImageButton>(R.id.btn_blangko) as ImageButton
+
+        btn_pilwali.setOnClickListener {
+            Toast.makeText(context, "Pilwali", Toast.LENGTH_SHORT).show()
+        }
+        btn_tps.setOnClickListener {
+            Toast.makeText(context, "TPS", Toast.LENGTH_SHORT).show()
+        }
+        btn_monitoring.setOnClickListener {
+            Toast.makeText(context, "Monitoring", Toast.LENGTH_SHORT).show()
+        }
+        btn_blangko.setOnClickListener {
+            Toast.makeText(context, "Blangko", Toast.LENGTH_SHORT).show()
+        }
+
+
+
+
+
+
+
+        return root
+    }
+
+    private fun openActivity(activity: Activity) {
+        startActivity(Intent(context, activity::class.java))
     }
 
     companion object {
