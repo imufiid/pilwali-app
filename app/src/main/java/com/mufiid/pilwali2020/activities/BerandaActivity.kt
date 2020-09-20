523b8f01f9e3974128689ce1609c7762192a0a7d
imufiid <imufiid98@gmail.com>
2020-09-20 21:23:59 +0700

    add check permission for SDK >= 23

--- app/src/main/java/com/mufiid/pilwali2020/activities/BerandaActivity.kt
+++ app/src/main/java/com/mufiid/pilwali2020/activities/BerandaActivity.kt
@@ -1,11 +1,15 @@
 package com.mufiid.pilwali2020.activities
 
+import android.Manifest
+import android.content.pm.PackageManager
+import android.os.Build
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.os.Handler
 import android.os.Looper
 import android.view.MenuItem
 import android.widget.Toast
+import androidx.core.app.ActivityCompat
 import androidx.fragment.app.Fragment
 import com.google.android.material.bottomnavigation.BottomNavigationView
 import com.mufiid.pilwali2020.R
@@ -21,11 +25,43 @@ class BerandaActivity : AppCompatActivity(), BottomNavigationView.OnNavigationIt
         supportActionBar?.hide()
         loadFragment(BerandaFragment())
         bottomNavigationView.setOnNavigationItemSelectedListener(this)
+
+        // check permission
+        if (Build.VERSION.SDK_INT >= 23) {
+            if (checkSelfPermission(
+                    Manifest.permission.ACCESS_FINE_LOCATION
+                ) != PackageManager.PERMISSION_GRANTED ||
+                checkSelfPermission(
+                    Manifest.permission.ACCESS_COARSE_LOCATION
+                ) != PackageManager.PERMISSION_GRANTED ||
+                checkSelfPermission(
+                    Manifest.permission.CAMERA
+                ) != PackageManager.PERMISSION_GRANTED ||
+                checkSelfPermission(
+                    Manifest.permission.READ_EXTERNAL_STORAGE
+                ) != PackageManager.PERMISSION_GRANTED ||
+                checkSelfPermission(
+                    Manifest.permission.WRITE_EXTERNAL_STORAGE
+                ) != PackageManager.PERMISSION_GRANTED
+            ) {
+                ActivityCompat.requestPermissions(
+                    this,
+                    arrayOf(
+                        android.Manifest.permission.ACCESS_FINE_LOCATION,
+                        android.Manifest.permission.ACCESS_COARSE_LOCATION,
+                        android.Manifest.permission.CAMERA,
+                        android.Manifest.permission.READ_EXTERNAL_STORAGE,
+                        android.Manifest.permission.WRITE_EXTERNAL_STORAGE
+                    ),
+                    1
+                )
+            }
+        }
     }
 
     override fun onNavigationItemSelected(item: MenuItem): Boolean {
         when (item.itemId) {
-            R.id.nav_home ->{
+            R.id.nav_home -> {
                 loadFragment(BerandaFragment())
                 return true
             }
