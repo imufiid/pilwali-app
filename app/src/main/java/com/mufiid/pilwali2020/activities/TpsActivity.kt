585c5fbd74ac20288881e3548c3079f113ac23d2
imufiid <imufiid98@gmail.com>
2020-09-20 12:46:18 +0700

    change UI tps activity_tps.xml

--- app/src/main/java/com/mufiid/pilwali2020/activities/TpsActivity.kt
+++ app/src/main/java/com/mufiid/pilwali2020/activities/TpsActivity.kt
@@ -2,6 +2,9 @@ package com.mufiid.pilwali2020.activities
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.Menu
+import android.view.MenuItem
+import android.widget.Toast
 import com.mufiid.pilwali2020.R
 
 class TpsActivity : AppCompatActivity() {
@@ -10,5 +13,20 @@ class TpsActivity : AppCompatActivity() {
         setContentView(R.layout.activity_tps)
         supportActionBar?.setDisplayHomeAsUpEnabled(true)
         supportActionBar?.title = "TPS"
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+//        return super.onCreateOptionsMenu(menu)
+        menuInflater.inflate(R.menu.menu_refresh_tps, menu)
+        return true
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        when(item.itemId) {
+            R.id.refresh -> {
+                Toast.makeText(this, "Refresh", Toast.LENGTH_SHORT).show()
+            }
+        }
+        return super.onOptionsItemSelected(item)
     }
 }
