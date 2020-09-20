f9c98bab3462e556141cf4e7c4bed32cfbafb3ea
imufiid <imufiid98@gmail.com>
2020-09-20 21:23:32 +0700

    set event listener

--- app/src/main/java/com/mufiid/pilwali2020/activities/PilwaliActivity.kt
+++ app/src/main/java/com/mufiid/pilwali2020/activities/PilwaliActivity.kt
@@ -1,12 +1,24 @@
 package com.mufiid.pilwali2020.activities
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.mufiid.pilwali2020.R
+import kotlinx.android.synthetic.main.activity_pilwali.*
 
 class PilwaliActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_pilwali)
+
+        supportActionBar?.title = "Pilwali"
+
+        btn_add.setOnClickListener {
+            startActivity(Intent(this, AddVoteActivity::class.java))
+        }
+    }
+
+    private fun captureBlangko() {
+
     }
 }
