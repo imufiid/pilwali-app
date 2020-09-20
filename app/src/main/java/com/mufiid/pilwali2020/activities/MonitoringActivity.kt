b0dad19573e8791f7327b78a41433c224c2fead0
imufiid <imufiid98@gmail.com>
2020-09-20 08:08:47 +0700

    create activity edit profile and monitoring

---
+++ app/src/main/java/com/mufiid/pilwali2020/activities/MonitoringActivity.kt
@@ -0,0 +1,12 @@
+package com.mufiid.pilwali2020.activities
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.mufiid.pilwali2020.R
+
+class MonitoringActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_monitoring)
+    }
+}
--- app/src/main/java/com/mufiid/pilwali2020/api/IApiService.kt
+++ app/src/main/java/com/mufiid/pilwali2020/api/IApiService.kt
@@ -1,11 +1,15 @@
 package com.mufiid.pilwali2020.api
 
+import com.mufiid.pilwali2020.models.Paslon
+import com.mufiid.pilwali2020.models.Tps
 import com.mufiid.pilwali2020.models.User
 import com.mufiid.pilwali2020.responses.MessageResponse
 import com.mufiid.pilwali2020.responses.WrappedResponse
 import io.reactivex.rxjava3.core.Flowable
 import retrofit2.http.Field
+import retrofit2.http.GET
 import retrofit2.http.POST
+import retrofit2.http.Path
 
 interface IApiService {
 
@@ -15,4 +19,14 @@ interface IApiService {
         @Field("username") username: String,
         @Field("password") password: String
     ): Flowable<WrappedResponse<User>>
+
+    // GET Jumlah Pemilih
+    @GET("")
+    fun getCountVoter(
+        @Path("id_tps") id_tps: String?
+    ): Flowable<WrappedResponse<Tps>>
+
+    // GET Paslon
+    @GET("")
+    fun getPaslon(): Flowable<WrappedResponse<Paslon>>
 }
