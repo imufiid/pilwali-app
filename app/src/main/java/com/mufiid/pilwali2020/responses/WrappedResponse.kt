d761dc38cbd0abbab5611cd702bb1eb8fc433e49
imufiid <imufiid98@gmail.com>
2020-09-24 20:53:17 +0700

    update data type for status

--- app/src/main/java/com/mufiid/pilwali2020/responses/WrappedResponse.kt
+++ app/src/main/java/com/mufiid/pilwali2020/responses/WrappedResponse.kt
@@ -3,7 +3,7 @@ package com.mufiid.pilwali2020.responses
 import com.google.gson.annotations.SerializedName
 
 data class WrappedResponse<T>(
-    @SerializedName("status") var status: String? = null,
+    @SerializedName("status") var status: Int? = null,
     @SerializedName("message") var message: String? = null,
     @SerializedName("data") var data: T? = null
 )
