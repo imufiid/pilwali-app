213f74fd0dc3e0be1e97f4f82e7c311dbf8602ab
imufiid <imufiid98@gmail.com>
2020-09-24 20:55:15 +0700

    create feature open camera

--- app/src/main/java/com/mufiid/pilwali2020/activities/AddVoteActivity.kt
+++ app/src/main/java/com/mufiid/pilwali2020/activities/AddVoteActivity.kt
@@ -1,16 +1,75 @@
 package com.mufiid.pilwali2020.activities
 
+import android.Manifest
+import android.content.Intent
+import android.content.pm.PackageManager
+import android.graphics.Bitmap
+import android.os.Build
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.provider.MediaStore
+import androidx.core.app.ActivityCompat
 import com.mufiid.pilwali2020.R
 import kotlinx.android.synthetic.main.activity_add_vote.*
+import kotlinx.android.synthetic.main.activity_add_vote.open_camera
+import kotlinx.android.synthetic.main.activity_tps.*
 
 class AddVoteActivity : AppCompatActivity() {
+    private val REQUEST_IMAGE_CAPTURE = 1
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_add_vote)
 
         supportActionBar?.setDisplayHomeAsUpEnabled(true)
-        //rv_paslon.isNestedScrollingEnabled = false
+
+
+        open_camera.setOnClickListener {
+            captureBlangko()
+        }
+
+        getPermission()
+    }
+
+    private fun captureBlangko() {
+        Intent(MediaStore.ACTION_IMAGE_CAPTURE).also { takePictureIntent ->
+            takePictureIntent.resolveActivity(packageManager)?.also {
+                startActivityForResult(takePictureIntent, REQUEST_IMAGE_CAPTURE)
+            }
+        }
+    }
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+        if (requestCode == REQUEST_IMAGE_CAPTURE && resultCode == RESULT_OK) {
+            val imageBitmap = data?.extras?.get("data") as Bitmap
+            image_blangko.setImageBitmap(imageBitmap)
+        }
+    }
+
+    private fun getPermission() {
+        // check permission
+        if (Build.VERSION.SDK_INT >= 23) {
+            if (checkSelfPermission(
+                    Manifest.permission.CAMERA
+                ) != PackageManager.PERMISSION_GRANTED ||
+                checkSelfPermission(
+                    Manifest.permission.READ_EXTERNAL_STORAGE
+                ) != PackageManager.PERMISSION_GRANTED ||
+                checkSelfPermission(
+                    Manifest.permission.WRITE_EXTERNAL_STORAGE
+                ) != PackageManager.PERMISSION_GRANTED
+            ) {
+                ActivityCompat.requestPermissions(
+                    this,
+                    arrayOf(
+                        android.Manifest.permission.CAMERA,
+                        android.Manifest.permission.READ_EXTERNAL_STORAGE,
+                        android.Manifest.permission.WRITE_EXTERNAL_STORAGE
+                    ),
+                    1
+                )
+            }
+        }
     }
 }
