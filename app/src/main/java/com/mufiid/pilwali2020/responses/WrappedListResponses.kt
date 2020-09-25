b39a286a092b64ccd7eb48699c865c8fa8788389
imufiid <imufiid98@gmail.com>
2020-09-25 17:50:11 +0700

    change data type

--- app/src/main/java/com/mufiid/pilwali2020/responses/WrappedListResponses.kt
+++ app/src/main/java/com/mufiid/pilwali2020/responses/WrappedListResponses.kt
@@ -4,6 +4,6 @@ import com.google.gson.annotations.SerializedName
 
 data class WrappedListResponses<T>(
     @SerializedName("message") var message: String? = null,
-    @SerializedName("status") var status: String? = null,
+    @SerializedName("status") var status: Int? = null,
     @SerializedName("data") var data: List<T>? = null
 )
