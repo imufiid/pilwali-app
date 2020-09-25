f332a42759fa56902daba844aae16fd3ca8822bf
imufiid <imufiid98@gmail.com>
2020-09-25 17:48:19 +0700

    create models paslon

--- app/src/main/java/com/mufiid/pilwali2020/models/Paslon.kt
+++ app/src/main/java/com/mufiid/pilwali2020/models/Paslon.kt
@@ -1,3 +1,18 @@
 package com.mufiid.pilwali2020.models
 
-class Paslon {}
+import com.google.gson.annotations.SerializedName
+
+data class Paslon(
+
+	@field:SerializedName("nm_peserta")
+	val nmPeserta: String? = null,
+
+	@field:SerializedName("foto")
+	val foto: String? = null,
+
+	@field:SerializedName("id")
+	val id: String? = null,
+
+	@field:SerializedName("no_peserta")
+	val noPeserta: String? = null
+)
