bda7e5c866f1a07d031323e973529beaa5dd32d4
imufiid <imufiid98@gmail.com>
2020-09-29 11:10:14 +0700

    add new fun interface

--- app/src/main/java/com/mufiid/pilwali2020/views/ITpsView.kt
+++ app/src/main/java/com/mufiid/pilwali2020/views/ITpsView.kt
@@ -3,9 +3,12 @@ package com.mufiid.pilwali2020.views
 import com.mufiid.pilwali2020.models.Tps
 
 interface ITpsView {
-    fun isLoadingTps()
-    fun hideLoadingTps()
+    fun isLoadingTps(state: Int?)
+    fun hideLoadingTps(state: Int?)
 
     fun getDataTps(message: String?, data: Tps)
     fun failedGetDataTps(message: String?)
+
+    fun messageSuccess(message: String?)
+    fun messageFailed(message: String?)
 }
