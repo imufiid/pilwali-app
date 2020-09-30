23b7ea20602c7226398d7309518a8a7815cbdd7a
imufiid <imufiid98@gmail.com>
2020-09-30 14:43:20 +0700

    presenter user

--- app/src/main/java/com/mufiid/pilwali2020/presenters/UserPresenter.kt
+++ app/src/main/java/com/mufiid/pilwali2020/presenters/UserPresenter.kt
@@ -7,7 +7,7 @@ import io.reactivex.rxjava3.schedulers.Schedulers
 
 class UserPresenter(private val userView: IUserView) {
     fun getUserByID(id_user: String) {
-        userView.isLoadingUser()
+        userView.isLoadingUser(1)
         ApiClient.instance().getUserById(id_user)
             .subscribeOn(Schedulers.io())
             .observeOn(AndroidSchedulers.mainThread())
@@ -17,13 +17,35 @@ class UserPresenter(private val userView: IUserView) {
                         userView.getDataUser(it.message, it.data!!)
                     }
                     else -> {
-                         userView.failedGetDataUser(it.message)
+                         userView.failedMessage(it.message)
                     }
                 }
-                userView.hideLoadingUser()
+                userView.hideLoadingUser(1)
             }, {
-                userView.failedGetDataUser(it.message)
-                userView.hideLoadingUser()
+                userView.failedMessage(it.message)
+                userView.hideLoadingUser(1)
+            })
+
+    }
+
+    fun updateUser(id_user: String?, username: String?, nama: String?, passwordNew: String?) {
+        userView.isLoadingUser(2)
+        ApiClient.instance().updateProfile(id_user, username, nama, passwordNew)
+            .subscribeOn(Schedulers.io())
+            .observeOn(AndroidSchedulers.mainThread())
+            .subscribe({
+                when(it.status) {
+                    201 -> {
+                        userView.successMessage(it.message)
+                    }
+                    400 -> {
+                        userView.failedMessage(it.message)
+                    }
+                }
+                userView.hideLoadingUser(2)
+            },{
+                userView.failedMessage(it.message)
+                userView.hideLoadingUser(2)
             })
 
     }
