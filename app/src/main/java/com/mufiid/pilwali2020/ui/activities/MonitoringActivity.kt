eaf003a1c6a9cdf1f249eeb495bf15414fda85d1
imufiid <imufiid98@gmail.com>
2020-10-05 17:24:34 +0700

    web view monitoring

--- app/src/main/java/com/mufiid/pilwali2020/ui/activities/MonitoringActivity.kt
+++ app/src/main/java/com/mufiid/pilwali2020/ui/activities/MonitoringActivity.kt
@@ -1,12 +1,54 @@
 package com.mufiid.pilwali2020.ui.activities
 
+import android.annotation.SuppressLint
+import android.app.ProgressDialog
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.webkit.WebView
+import android.webkit.WebViewClient
 import com.mufiid.pilwali2020.R
+import com.mufiid.pilwali2020.utils.Constants
+import kotlinx.android.synthetic.main.activity_monitoring.*
 
+@Suppress("DEPRECATION")
 class MonitoringActivity : AppCompatActivity() {
+    companion object {
+        private var loading: ProgressDialog? = null
+    }
+
+    @SuppressLint("SetJavaScriptEnabled")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_monitoring)
+
+        supportActionBar?.title = "Monitoring Perhitungan"
+        loading = ProgressDialog(this)
+        // set web view
+        wv_monitoring.loadUrl(Constants.URL_WEBVIEW)
+        wv_monitoring.settings.apply {
+            javaScriptEnabled = true
+        }
+        wv_monitoring.webViewClient = WebViewClient()
+
+        // ....
+        loading?.setMessage("Tunggu sebentar...")
+        loading?.show()
+    }
+
+    open class WebViewClient : android.webkit.WebViewClient() {
+        override fun shouldOverrideUrlLoading(
+            view: WebView,
+            url: String
+        ): Boolean {
+            view.loadUrl(url)
+            return true
+        }
+
+        override fun onPageFinished(view: WebView, url: String) {
+            super.onPageFinished(view, url)
+            //loading?.dismiss()
+            loading?.dismiss()
+        }
     }
 }
