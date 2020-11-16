30ec77b6892c2268a3aa0b805b1078938601edee
imufiid <imufiid98@gmail.com>
2020-11-16 09:21:38 +0700

    change preference with core ktx

--- app/src/main/java/com/mufiid/pilwali2020/utils/Constants.kt
+++ app/src/main/java/com/mufiid/pilwali2020/utils/Constants.kt
@@ -1,13 +1,14 @@
 package com.mufiid.pilwali2020.utils
 
 import android.content.Context
+import androidx.core.content.edit
 
 class Constants {
     companion object {
-        const val API_ENDPOINT = "http://tobiaditia.my.id/Api/"
-        const val URL_WEBVIEW = "http://tobiaditia.my.id/publics/chart_perhitungan"
+        const val API_ENDPOINT = "http://pilkada2020.blitarkota.go.id/Api/"
+        const val URL_WEBVIEW = "http://pilkada2020.blitarkota.go.id/publics/chart_perhitungan"
 
-        fun getUsername(context: Context) : String? {
+        fun getUsername(context: Context): String? {
             val pref = context.getSharedPreferences("USER", Context.MODE_PRIVATE)
             return pref.getString("USERNAME", "undefined")
         }
@@ -20,74 +21,71 @@ class Constants {
             }
         }
 
-        fun getApiKey(context: Context) : String? {
+        fun getApiKey(context: Context): String? {
             val pref = context.getSharedPreferences("USER", Context.MODE_PRIVATE)
             return pref.getString("APIKEY", "undefined")
         }
 
         fun setApiKey(context: Context, apiKey: String) {
             val pref = context.getSharedPreferences("USER", Context.MODE_PRIVATE)
-            pref.edit().apply {
+            pref.edit {
                 putString("APIKEY", apiKey)
-                apply()
             }
         }
 
-        fun getIDUser(context: Context) : String? {
+        fun getIDUser(context: Context): String? {
             val pref = context.getSharedPreferences("USER", Context.MODE_PRIVATE)
             return pref.getString("ID_USER", "undefined")
         }
 
         fun setIDTps(context: Context, username: String) {
             val pref = context.getSharedPreferences("USER", Context.MODE_PRIVATE)
-            pref.edit().apply {
+            pref.edit {
                 putString("ID_TPS", username)
-                apply()
             }
         }
 
-        fun getVerification(context: Context?) : Int? {
+        fun getVerification(context: Context?): Int? {
             val pref = context?.getSharedPreferences("USER", Context.MODE_PRIVATE)
             return pref?.getInt("VERIF", 0)
         }
 
         fun setVerification(context: Context, verification: Int) {
             val pref = context.getSharedPreferences("USER", Context.MODE_PRIVATE)
-            pref.edit().apply {
+            pref.edit {
                 putInt("VERIF", verification)
-                apply()
             }
         }
 
-        fun getIDTps(context: Context) : String? {
+        fun getIDTps(context: Context): String? {
             val pref = context.getSharedPreferences("USER", Context.MODE_PRIVATE)
             return pref.getString("ID_TPS", "undefined")
         }
 
         fun setIDUser(context: Context, username: String) {
             val pref = context.getSharedPreferences("USER", Context.MODE_PRIVATE)
-            pref.edit().apply {
+            pref.edit {
                 putString("ID_USER", username)
-                apply()
             }
         }
 
-        fun ISLOGGEDIN(context: Context) : Boolean {
+        fun ISLOGGEDIN(context: Context): Boolean {
             val pref = context.getSharedPreferences("USER", Context.MODE_PRIVATE)
             return pref.getBoolean("ISLOGGEDIN", false)
         }
 
         fun setISLOGGEDIN(context: Context, state: Boolean) {
             val pref = context.getSharedPreferences("USER", Context.MODE_PRIVATE)
-            pref.edit().apply {
+            pref.edit {
                 putBoolean("ISLOGGEDIN", state)
-                apply()
             }
         }
 
         fun clear(context: Context) {
             val pref = context.getSharedPreferences("USER", Context.MODE_PRIVATE)
-            pref.edit().clear().apply()
+            pref.edit{
+                clear()
+            }
         }
 
         fun isValidPhone(phone: String) = phone.length >= 12
