5dd042627351ba21a973cdacc64e1259e51586cc
imufiid <imufiid98@gmail.com>
2020-11-16 11:34:01 +0700

    change to var and add parcelable

--- app/src/main/java/com/mufiid/pilwali2020/models/User.kt
+++ app/src/main/java/com/mufiid/pilwali2020/models/User.kt
@@ -1,41 +1,44 @@
 package com.mufiid.pilwali2020.models
 
+import android.os.Parcelable
 import com.google.gson.annotations.SerializedName
+import kotlinx.android.parcel.Parcelize
 
+@Parcelize
 data class User(
     @field:SerializedName("password")
-    val password: String? = null,
+    var password: String? = null,
 
     @field:SerializedName("is_active")
-    val isActive: String? = null,
+    var isActive: String? = null,
 
     @field:SerializedName("nama")
-    val nama: String? = null,
+    var nama: String? = null,
 
     @field:SerializedName("no_hp")
-    val noHp: String? = null,
+    var noHp: String? = null,
 
     @field:SerializedName("jabatan")
-    val jabatan: String? = null,
+    var jabatan: String? = null,
 
     @field:SerializedName("id_tps")
-    val idTps: String? = null,
+    var idTps: String? = null,
 
     @field:SerializedName("id_wilayah")
-    val idWilayah: String? = null,
+    var idWilayah: String? = null,
 
     @field:SerializedName("id_role")
-    val idRole: String? = null,
+    var idRole: String? = null,
 
     @field:SerializedName("id")
-    val id: String? = null,
+    var id: String? = null,
 
     @field:SerializedName("date_registered")
-    val dateRegistered: Any? = null,
+    var dateRegistered: String? = null,
 
     @field:SerializedName("username")
-    val username: String? = null,
+    var username: String? = null,
 
     @field:SerializedName("api_key")
-    val api_key: String? = null
-)
+    var api_key: String? = null
+): Parcelable
