318f812feae4391e77e62447670b7210d49bb545
imufiid <imufiid98@gmail.com>
2020-11-16 11:34:29 +0700

    add sharedPreference with model user

--- app/src/main/java/com/mufiid/pilwali2020/utils/Constants.kt
+++ app/src/main/java/com/mufiid/pilwali2020/utils/Constants.kt
@@ -2,11 +2,32 @@ package com.mufiid.pilwali2020.utils
 
 import android.content.Context
 import androidx.core.content.edit
+import com.mufiid.pilwali2020.models.User
 
 class Constants {
     companion object {
         const val API_ENDPOINT = "http://pilkada2020.blitarkota.go.id/Api/"
         const val URL_WEBVIEW = "http://pilkada2020.blitarkota.go.id/publics/chart_perhitungan"
+
+        fun getUserData(context: Context): User? {
+            val pref = context.getSharedPreferences("USER", Context.MODE_PRIVATE)
+            return User().apply {
+                id = pref.getString("ID_USER", "undefined")
+                username = pref.getString("USERNAME", "undefined")
+                api_key = pref.getString("APIKEY", "undefined")
+                idTps = pref.getString("ID_TPS", "undefined")
+            }
+        }
+
+        fun setUserData(context: Context, user: User) {
+            val pref = context.getSharedPreferences("USER", Context.MODE_PRIVATE)
+            pref.edit {
+                putString("USERNAME", user.username)
+                putString("APIKEY", user.api_key)
+                putString("ID_USER", user.id)
+                putString("ID_TPS", user.idTps)
+            }
+        }
 
         fun getUsername(context: Context): String? {
             val pref = context.getSharedPreferences("USER", Context.MODE_PRIVATE)
