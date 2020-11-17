a645430fff625a3b96a891a06f6df406e66d92d7
imufiid <imufiid98@gmail.com>
2020-11-17 14:48:34 +0700

    clean code

--- app/src/main/java/com/mufiid/pilwali2020/ui/fragments/ProfilFragment.kt
+++ app/src/main/java/com/mufiid/pilwali2020/ui/fragments/ProfilFragment.kt
@@ -16,26 +16,7 @@ import com.mufiid.pilwali2020.ui.activities.LoginActivity
 import com.mufiid.pilwali2020.utils.Constants
 import org.w3c.dom.Text
 
-private const val ARG_PARAM1 = "param1"
-private const val ARG_PARAM2 = "param2"
-
-/**
- * A simple [Fragment] subclass.
- * Use the [ProfilFragment.newInstance] factory method to
- * create an instance of this fragment.
- */
 class ProfilFragment : Fragment() {
-    // TODO: Rename and change types of parameters
-    private var param1: String? = null
-    private var param2: String? = null
-
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        arguments?.let {
-            param1 = it.getString(ARG_PARAM1)
-            param2 = it.getString(ARG_PARAM2)
-        }
-    }
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -52,47 +33,28 @@ class ProfilFragment : Fragment() {
         val tvUsername = view.findViewById<TextView>(R.id.username_user) as TextView
         val appVersion = view.findViewById<TextView>(R.id.app_version)
 
-        appVersion.text = "App Version - ${BuildConfig.VERSION_NAME}"
+        appVersion.text = "${getString(R.string.app_versions, BuildConfig.VERSION_NAME)} \n ${getString(R.string.copyright)}"
 
         editProfile.setOnClickListener {
             startActivity(Intent(context, EditProfileActivity::class.java))
         }
 
-        tvUsername.text = Constants.getUsername(context!!)
+        tvUsername.text = Constants.getUserData(context!!)?.username
 
         logout.setOnClickListener {
             AlertDialog.Builder(context).apply {
-                setTitle("Konfirmasi keluar")
-                setMessage("Anda yakin keluar?")
-                    .setPositiveButton("Iya") { dialogInterface, i ->
+                setTitle(getString(R.string.confirm_logout))
+                setMessage(getString(R.string.question_logout))
+                    .setPositiveButton(getString(R.string.yes)) { _, _->
                         Constants.clear(context!!)
                         startActivity(Intent(context, LoginActivity::class.java))
                         activity?.finish()
                     }
-                    .setNegativeButton("Tidak") { dialogInterface, i ->
+                    .setNegativeButton(getString(R.string.no)) { dialogInterface, _ ->
                         dialogInterface.dismiss()
                     }
             }.show()
 
         }
-    }
-
-    companion object {
-        /**
-         * Use this factory method to create a new instance of
-         * this fragment using the provided parameters.
-         *
-         * @param param1 Parameter 1.
-         * @param param2 Parameter 2.
-         * @return A new instance of fragment ProfilFragment.
-         */
-        @JvmStatic
-        fun newInstance(param1: String, param2: String) =
-            ProfilFragment().apply {
-                arguments = Bundle().apply {
-                    putString(ARG_PARAM1, param1)
-                    putString(ARG_PARAM2, param2)
-                }
-            }
     }
 }
