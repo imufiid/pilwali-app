0d669de02959f22b40073b645fc6a67dd1caf9b4
imufiid <imufiid98@gmail.com>
2021-11-07 11:40:51 +0700

    [refactor]-create auth view model

---
+++ app/src/main/java/com/mufiid/pilwali2020/ui/login/AuthViewModel.kt
@@ -0,0 +1,7 @@
+package com.mufiid.pilwali2020.ui.login
+
+import androidx.lifecycle.ViewModel
+import com.mufiid.pilwali2020.data.repository.AuthRepository
+
+class AuthViewModel(private val authRepo: AuthRepository): ViewModel() {
+}
